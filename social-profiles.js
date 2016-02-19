5c39829f2c937376e00668139b3dba6cee42a624
jafaircl <jafaircl@me.com>
2016-02-19 15:49:15 -0500

    Create social-profiles.js

---
+++ social-profiles.js
@@ -0,0 +1,13 @@
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Organization",
+  "name" : "Your Organization Name",
+  "url" : "http://www.your-site.com",
+  "sameAs" : [
+    "http://www.facebook.com/your-profile",
+    "http://www.twitter.com/yourProfile",
+    "http://plus.google.com/your_profile"
+  ]
+}
+</script>
