a6cf8790043e450cae4e9a0779b4a31703fbc450
jafaircl <jafaircl@me.com>
2016-02-19 16:54:46 -0500

    Create breadcrumbs.php

---
+++ breadcrumbs.php
@@ -0,0 +1,82 @@
+<!-- Single breadcrumb trail -->
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "https://example.com/arts",
+      "name": "Arts"
+    }
+  },{
+    "@type": "ListItem",
+    "position": 2,
+    "item": {
+      "@id": "https://example.com/arts/books",
+      "name": "Books"
+    }
+  },{
+    "@type": "ListItem",
+    "position": 3,
+    "item": {
+      "@id": "https://example.com/arts/books/poetry",
+      "name": "Poetry"
+    }
+  }]
+}
+</script>
+
+<!-- Multiple breadcrumb trails -->
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "https://example.com/arts",
+      "name": "Arts"
+    }
+  },{
+    "@type": "ListItem",
+    "position": 2,
+    "item": {
+      "@id": "https://example.com/arts/books",
+      "name": "Books"
+    }
+  },{
+    "@type": "ListItem",
+    "position": 3,
+    "item": {
+      "@id": "https://example.com/arts/books/poetry",
+      "name": "Poetry"
+    }
+  }]
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "https://example.com/literature",
+      "name": "Literature"
+    }
+  },{
+    "@type": "ListItem",
+    "position": 2,
+    "item": {
+      "@id": "https://example.com/literature/poetry",
+      "name": "Poetry"
+    }
+  }]
+}
+</script>
