cc5bd0ef23866ef63b58bc38cc8c2e411c03e25d
jafaircl <jafaircl@me.com>
2016-02-22 10:20:47 -0500

    Update article.php

--- article.php
+++ article.php
@@ -33,89 +33,11 @@
 }
 </script>
 
-<!-- With a WordPress plugin that defines an option for "logo" -->
-
-<script type="application/ld+json">
-    {
-  "@context": "http://schema.org",
-  "@type": "NewsArticle",
-  "mainEntityOfPage":{
-    "@type":"WebPage",
-    "@id":"https://google.com/article"
-  },
-  "headline": "Article headline",
-  "image": {
-    "@type": "ImageObject",
-    "url": "https://google.com/thumbnail1.jpg",
-    "height": 800,
-    "width": 800
-  },
-  "datePublished": "2015-02-05T08:00:00+08:00",
-  "dateModified": "2015-02-05T09:20:00+08:00",
-  "author": {
-    "@type": "Person",
-    "name": "John Doe"
-  },
-   "publisher": {
-    "@type": "Organization",
-    "name": "Google",
-    "logo": {
-      "@type": "ImageObject",
-      "url": "https://google.com/logo.jpg",
-      "width": 600,
-      "height": 60
-    }
-  },
-  "description": "A most wonderful article"
-}
- </script>
- 
-<!-- Now with WordPress using the header image as the logo. Theme must support header image. -->
-
-<?php // Get the header image. Returns a url.
-    $img_url = get_header_image();
-?>
-
-<script type="application/ld+json">
-  {
-  "@context": "http://schema.org",
-  "@type": "NewsArticle",
-  "mainEntityOfPage":{
-    "@type":"WebPage",
-    "@id":"https://google.com/article"
-  },
-  "headline": "Article headline",
-  "image": {
-    "@type": "ImageObject",
-    "url": "https://google.com/thumbnail1.jpg",
-    "height": 800,
-    "width": 800
-  },
-  "datePublished": "2015-02-05T08:00:00+08:00",
-  "dateModified": "2015-02-05T09:20:00+08:00",
-  "author": {
-    "@type": "Person",
-    "name": "John Doe"
-  },
-   "publisher": {
-    "@type": "Organization",
-    "name": "Google",
-    "logo": {
-      "@type": "ImageObject",
-      "url": "https://google.com/logo.jpg",
-      "width": 600,
-      "height": 60
-    }
-  },
-  "description": "A most wonderful article"
-}
- </script> 
-
-<!-- Now with WordPress using the featured image as the logo -->
+<!-- Now with WordPress using common hooks & plugin-defined logo -->
 
 <?php // Get the featured image. Returns an array: [url, width, height]
     $img_id = get_post_thumbnail_id();
-    $img_url = wp_get_attachment_image( $img_id, 'full', true );
+    $img_url = wp_get_attachment_image( $img_id, 'full', true ); // Returns an array: [url, width, height]
 ?>
 
 <script type="application/ld+json">
@@ -124,31 +46,32 @@
   "@type": "NewsArticle",
   "mainEntityOfPage":{
     "@type":"WebPage",
-    "@id":"https://google.com/article"
+    "@id":"<?php the_permalink(); ?>"
   },
   "headline": "Article headline",
   "image": {
     "@type": "ImageObject",
-    "url": "https://google.com/thumbnail1.jpg",
-    "height": 800,
-    "width": 800
+    "url": "<?php echo $img_url[0]; ?>",
+    "width": "<?php echo $img_url[1]; ?>",
+    "height": "<?php echo $img_url[2]; ?>"
   },
-  "datePublished": "2015-02-05T08:00:00+08:00",
-  "dateModified": "2015-02-05T09:20:00+08:00",
+  "datePublished": "<?php echo get_the_date('c'); ?>",
+  "dateModified": "<?php echo get_the_modified_date('c'); ?>",
   "author": {
     "@type": "Person",
-    "name": "John Doe"
+    "name": "<?php the_author_meta('display_name', 1); ?>"
   },
    "publisher": {
     "@type": "Organization",
-    "name": "Google",
+    "name": "<?php bloginfo('name'); ?>",
     "logo": {
       "@type": "ImageObject",
-      "url": "https://google.com/logo.jpg",
-      "width": 600,
-      "height": 60
+      "url": "<?php echo get_option('logo'); ?>",
+      <?php list($width, $height, $type, $attr) = getimagesize( get_option('logo') ); ?>
+      "width": "<? echo $width; ?>",
+      "height": "<? echo $height; ?>"
     }
   },
-  "description": "A most wonderful article"
+  "description": "<?php $myExcerpt = get_the_excerpt(); $tags = array("<p>", "</p>"); $myExcerpt = str_replace($tags, "", $myExcerpt); echo $myExcerpt; ?>"
 }
  </script>
