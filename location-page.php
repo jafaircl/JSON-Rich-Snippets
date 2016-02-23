9364cbaa7f2bc8fd58d5c698ec364dffd3b1ad8b
jafaircl <jafaircl@me.com>
2016-02-23 15:34:47 -0500

    Create location-page.php

---
+++ location-page.php
@@ -0,0 +1,76 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "MedicalOrganization",
+  "@id": "https://essentiaservices.org/",
+  "name": "West Acres Clinic (Fargo)",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "3902 13th Avenue South",
+    "addressLocality": "Fargo",
+    "addressRegion": "ND",
+    "postalCode": "58103",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 40.761293,
+    "longitude": -73.982294
+  },
+  "url": "https://essentiaservices.org/location/west-acres-clinic-fargo/",
+  "telephone": "+17013648900",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "08:00",
+      "closes": "20:30"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Saturday",
+      "opens": "09:00",
+      "closes": "16:30"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Sunday",
+      "opens": "12:00",
+      "closes": "16:30"
+    }
+  ],
+  "memberOf": {
+    "@type": "Organization",
+    "url": "https://essentiaservices.org/",
+    "logo": {
+        "@type": "ImageObject",
+        "url": "https://essentiaservices.org/wp-content/themes/essentia/images/mark.png",
+        "width": "450",
+        "height": "94"
+      }
+  },
+  "potentialAction":{
+    "@type":"ReserveAction",
+    "target":{
+      "@type":"EntryPoint",
+      "urlTemplate":"https://essentiaservices.org/request-an-appointment/",
+      "inLanguage":"en-US",
+      "actionPlatform":[
+        "http://schema.org/DesktopWebPlatform",
+        "http://schema.org/IOSPlatform",
+        "http://schema.org/AndroidPlatform"
+      ]
+    },
+    "result":{
+      "@type":"Reservation",
+      "name":"Request an appointment"
+    }
+  }
+}
+</script>
